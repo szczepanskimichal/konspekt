0e59aff1b98e56c72f5899d365369ead8ee1feec
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-04 16:48:25 +0100

    cd

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,69 +1,12 @@
-// export default function Test() {
-//   return <div>Test</div>;
-// }
-// import { tab } from "@testing-library/user-event/dist/tab";
-// import React, { Component } from "react";
-
-// class Test extends Component {
-// constructor(props) {
-//   super(props);
-//   console.log(this);
-//   this.foo = this.foo.bind(this);
-// }
-// xyz = function () {
-//   console.log("hei jeg heter: ", this.props.x);
-// };
-
-// // foo(name) {
-// //   // event.preventDefault();
-// //   console.log("Hei, Jeg heter", this.props.x);
-// // }
-// render() {
-//   console.log(this);
-//   return <button onCLick={this.xyz}>Click</button>;
-
-// <button onClick={(event) => this.foo("Cziczi", event)}>Click</button>
-// constructor(props) {
-//   super(props);
-
-//   this.state = {
-//     counter: 1,
-//     name: "Michal",
-//     tab: [1, 2, 3, 4],
-//   };
-// }
-// increment = () => {
-//   console.log("increment", this.state.counter);
-
-//   // this.setState({
-//   //   counter: "nyStart",
-//   // });
-//   this.setState((prevState) => {
-//     return {
-//       counter: prevState.counter + 1,
-//     };
-//   });
-
-//   this.setState((prevState) => {
-//     return {
-//       tab: [...prevState.tab, 5],
-//     };
-//   });
-//   console.log("increment", this.state.counter);
-//   this.setState(
-//     {
-//       name: "Cziczi",
-//     },
-//     () => {
-//       console.log("after state update:", this.state.name);
-//     }
-//   );
 import React, { Component } from "react";
 
-function Child({ user }) {
+function Child({ user, foo }) {
+  const handleClick = () => {
+    foo(" message to parent from child", user);
+  };
   return (
     <div>
-      {user} - <button>REMOVE</button>
+      {user} - <button onClick={foo}>REMOVE</button>
     </div>
   );
 }
@@ -76,17 +19,22 @@ class Parent extends Component {
       list: ["ada", "mija", "michal"],
     };
   }
-}
-render(){
-  console.log("render parent");
-  const { list } = this.state;
-  return (
-    <>
-      {list.map((user) => (
-        <Child key={user} user={user} />
-      ))}
-    </>
-  );
+
+  send = (msg) => {
+    console.log("message from parent");
+  };
+
+  render() {
+    console.log("render parent");
+    const { list } = this.state;
+    return (
+      <>
+        {list.map((user) => (
+          <Child key={user} user={user} foo={this.send} />
+        ))}
+      </>
+    );
+  }
 }
 
 export default Parent;
