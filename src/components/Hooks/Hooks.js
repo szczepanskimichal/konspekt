bd62a46399278f6e90030c7e4aeb317ca507fb12
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-06 09:25:07 +0100

    cd...

--- src/components/Hooks/Hooks.js
+++ src/components/Hooks/Hooks.js
@@ -2,15 +2,15 @@ import { Button } from "@mui/material";
 import { useState } from "react";
 
 export default function Test() {
+  console.log(0);
   const [counter, setCounter] = useState(0);
-
-  const hamdleClick = () => {
-    setCounter(1234);
-  };
+  //   const hamdleClick = () => {
+  //     setCounter(1234);
+  //   };
   return (
     <>
-      <Button onClick={hamdleClick} variant="contained" color="secondary">
-        Click Me {counter}
+      <Button variant="contained" color="secondary">
+        Click Me
       </Button>
     </>
   );
